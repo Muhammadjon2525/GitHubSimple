2939c81cddb12c66f03bd40079abc71cd60c49a0
mehriddin <uzeconomist0419@gmail.com>
2023-05-23 13:44:25 +0500

    first

--- app/src/main/java/uz/gita/githubsimple/MainActivity.kt
+++ app/src/main/java/uz/gita/githubsimple/MainActivity.kt
@@ -2,12 +2,14 @@ package uz.gita.githubsimple
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("TTT", "onCreate: ")
     }
 
 }
